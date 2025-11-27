826c720b53ea5e3514507cb91e81325e7ebfa7b8
sahilraj87083 <sahilraj87093@gmail.com>
2025-11-27 16:06:19 +0530

    implement main currency converter logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,87 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {InputBox} from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [fromCurrency, setFromCurrency] = useState('usd')
+  const [toCurrency, setToCurrency] = useState('inr')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(fromCurrency)
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFromCurrency(toCurrency)
+    setToCurrency(fromCurrency)
+
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[toCurrency])
+  }
+  
 
   return (
-    <div className='bg-gray-500'>
-      <h1>hi </h1>
-    </div>
-  )
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('https://plus.unsplash.com/premium_photo-1681487767138-ddf2d67b35c1?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y3VycmVuY3klMjBleGNoYW5nZXxlbnwwfHwwfHx8MA%3D%3D')`,
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                           convert();
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+
+                                label="From"
+                                amount={amount}
+                                currencyOptions={options}
+                                onCurrencyChange={(currency) => setAmount(amount)}
+                                selectedCurrency={fromCurrency}
+                                onAmountChange={(amount) => setAmount(amount)}
+                                
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                onClick={swap}
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                amount={convertedAmount}
+                                currencyOptions={options}
+                                onCurrencyChange={(currency) => setToCurrency(currency)}
+                                selectedCurrency={toCurrency}
+                                amountDisabled 
+                                
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert {fromCurrency.toUpperCase()} to {toCurrency.toUpperCase()}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,4 +1,4 @@
-import { use, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 
 function useCurrencyInfo(currency){
     const [currencyInfo, setCurrencyInfo] = useState({});
